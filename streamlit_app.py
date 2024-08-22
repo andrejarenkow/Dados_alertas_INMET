03a11de17d21ef5275ba90c92ba4251fd6ce7a4a
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-08-22 13:24:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,19 @@
+# Importação da biblioteca
+import sqlitecloud
 import streamlit as st
 
 st.write('Hello')
+
+# Conexão com o banco
+conn = sqlitecloud.connect('sqlitecloud://cajzrljjsz.sqlite.cloud:8860?apikey=JVcs3H4XeiK8G81N0hT1xjtpwRWBb2DFu8yvNXHmjXM')
+
+engine = create_engine(f'sqlite:///{PATH_TO_BD}')
+Base.metadata.create_all(bind = engine)
+
+# Nome da database
+db_name = 'bd_alertas.sqlite'
+conn.execute(f'USE DATABASE {db_name}')
+
+dados = pd.read_sql('SELECT * FROM alertas LIMIT 20', conn)
+
+dados
